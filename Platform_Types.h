87413955e60def339c637f2e1c9da8564e2dfa90
Nikolay Dimitrow <nikidimitrow@users.noreply.github.com>
2022-06-29 22:24:13 +0300

    Create Platform_Types.h

---
+++ Platform_Types.h
@@ -0,0 +1,86 @@
+#ifndef PLATFORM_TYPES_H
+#define PLATFORM_TYPES_H
+
+/**********************************************************************************************************************
+ * INCLUDES
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  GLOBAL CONSTANT MACROS
+ *********************************************************************************************************************/
+ 
+/* AUTOSAR Software Specification Document Version Information */
+
+/* AUTOSAR release 4.0 R3 */
+#define PLATFORM_AR_RELEASE_MAJOR_VERSION      (4u)
+#define PLATFORM_AR_RELEASE_MINOR_VERSION      (0u)
+#define PLATFORM_AR_RELEASE_REVISION_VERSION   (3u)
+
+/* Component Version Information */
+#define PLATFORM_SW_MAJOR_VERSION       (2u)
+#define PLATFORM_SW_MINOR_VERSION       (1u)
+#define PLATFORM_SW_PATCH_VERSION       (1u)
+
+#define CPU_TYPE_8       8u
+#define CPU_TYPE_16      16u
+#define CPU_TYPE_32      32u
+
+#define MSB_FIRST        0u  /* big endian bit ordering */
+#define LSB_FIRST        1u  /* little endian bit ordering */
+
+#define HIGH_BYTE_FIRST  0u  /* big endian byte ordering */
+#define LOW_BYTE_FIRST   1u  /* little endian byte ordering */
+
+#ifndef TRUE
+  #define TRUE   1u
+#endif
+
+#ifndef FALSE
+  #define FALSE  0u
+#endif
+
+#define CPU_TYPE            CPU_TYPE_32
+#define CPU_BIT_ORDER       LSB_FIRST
+#define CPU_BYTE_ORDER      LOW_BYTE_FIRST
+
+/**********************************************************************************************************************
+ *  GLOBAL FUNCTION MACROS
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  GLOBAL DATA TYPES AND STRUCTURES
+ *********************************************************************************************************************/
+typedef unsigned char       boolean;        /* for use with TRUE/FALSE        */
+
+typedef signed char         sint8;          /*        -128 .. +127            */
+typedef unsigned char       uint8;          /*           0 .. 255             */
+typedef signed short        sint16;         /*      -32768 .. +32767          */
+typedef unsigned short      uint16;         /*           0 .. 65535           */
+typedef signed long         sint32;         /* -2147483648 .. +2147483647     */
+typedef unsigned long       uint32;         /*           0 .. 4294967295      */
+#define PLATFORM_SUPPORT_SINT64_UINT64
+typedef signed long long    sint64;         /* \brief 64-bit unsigned integer */
+typedef unsigned long long  uint64;
+typedef unsigned long       uint8_least;    /* At least 8 bit                 */
+typedef unsigned long       uint16_least;   /* At least 16 bit                */
+typedef unsigned long       uint32_least;   /* At least 32 bit                */
+typedef signed long         sint8_least;    /* At least 7 bit + 1 bit sign    */
+typedef signed long         sint16_least;   /* At least 15 bit + 1 bit sign   */
+typedef signed long         sint32_least;   /* At least 31 bit + 1 bit sign   */
+
+typedef float               float32;
+typedef double              float64;
+
+/**********************************************************************************************************************
+ *  GLOBAL DATA PROTOTYPES
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  GLOBAL FUNCTION PROTOTYPES
+ *********************************************************************************************************************/
+
+#endif /* PLATFORM_TYPES_H */
+
+/**********************************************************************************************************************
+ *  END OF FILE: Platform_Types.h
+ *********************************************************************************************************************/
